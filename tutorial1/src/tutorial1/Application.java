ce24572d43f6d03522e7e9377e647fe1b07b0b15
John Rykken <john.rykken@gmail.com>
2018-11-01 08:36:12 -0700

    Add tutorial 4.

--- tutorial1/src/tutorial1/Application.java
+++ tutorial1/src/tutorial1/Application.java
@@ -13,7 +13,7 @@ public class Application {
 		double doubleNum = 99.6667;
 		// floats are short doubles, f must be on the end
 		float floatNum = 22.1f;
-		// single character
+		// single character 
 		char someChar = 'y';
 		boolean myBool = true;
 		byte someByte = 123;
---
+++ tutorial4/src/tutorial4/WhileLoopPractice.java
@@ -0,0 +1,15 @@
+package tutorial4;
+
+public class WhileLoopPractice {
+	
+	public static void main(String[] args) {
+		int value = 5;
+		
+		while(value <  55) 
+		{
+		 	System.out.println("Value is " + value);
+			value = value + 2;
+		}
+	}
+	
+}
